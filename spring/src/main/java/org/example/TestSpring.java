e613f9ae2be08e99991adf51940c6c1520720680
Samarskiyghoul <118137484+Samarskiyghoul@users.noreply.github.com>
2023-04-16 15:41:58 +0400

    Add files via upload

---
+++ spring/src/main/java/org/example/TestSpring.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        MusicPlayer musicPlayer = context.getBean("musicListBean", MusicPlayer.class);
+        musicPlayer.playMusic();
+
+
+
+        context.close();
+
+    }
+
+
+}
